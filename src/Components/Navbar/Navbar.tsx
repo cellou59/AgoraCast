9971f54308ccc4a10f569758194d7e00a18fd039
cellouRetailShake <cellu@retailshake.com>
2024-07-15 16:31:24 +0200

    feat: 🎸 get video from api in play video
    - Update PlayVideo component CSS to use an iframe instead of a video
    element for better compatibility with different video sources

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,36 +1,46 @@
-import React from 'react'
-import './Navbar.css'
-import menu_icon from '../../assets/menu.png'
-import logo from '../../assets/logo.png'
-import search_icon from '../../assets/search.png'
-import upload_icon from '../../assets/upload.png'
-import more_icon from '../../assets/more.png'
-import notification_icon from '../../assets/notification.png'
-import profile_icon from '../../assets/jack.png'
+import React from "react";
+import "./Navbar.css";
+import menu_icon from "../../assets/menu.png";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search.png";
+import upload_icon from "../../assets/upload.png";
+import more_icon from "../../assets/more.png";
+import notification_icon from "../../assets/notification.png";
+import profile_icon from "../../assets/jack.png";
+import { Link } from "react-router-dom";
 
-function Navbar({setSidebarOpen}:{setSidebarOpen:React.Dispatch<React.SetStateAction<boolean>>}) {
+function Navbar({
+  setSidebarOpen,
+}: {
+  setSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   return (
-    
     <nav className="flex-div">
-        <div className="nav-left flex-div">
-            <img className='menu-icon' src={menu_icon} onClick={() =>setSidebarOpen((prev:boolean) => prev === false)} alt="" />
-            <img className='logo' src={logo} alt="" />
+      <div className="nav-left flex-div">
+        <img
+          className="menu-icon"
+          src={menu_icon}
+          onClick={() => setSidebarOpen((prev: boolean) => prev === false)}
+          alt=""
+        />
+        <Link to="/">
+          <img className="logo" src={logo} alt="" />
+        </Link>
+      </div>
+      <div className="nav-middle flex-div">
+        <div className="search-box flex-div">
+          <input type="text" placeholder="Search" />
+          <img src={search_icon} alt="" />
         </div>
-        <div className="nav-middle flex-div">
-            <div className="search-box flex-div">
-                <input type="text" placeholder='Search'/>
-                <img src={search_icon} alt="" />
-
-            </div>
-        </div>
-        <div className="nav-right flex-div">
-            <img src={upload_icon} alt="" />
-            <img src={more_icon} alt="" />
-            <img src={notification_icon} alt="" />
-            <img src={profile_icon} className='user-icon'  alt="" />
-        </div>
+      </div>
+      <div className="nav-right flex-div">
+        <img src={upload_icon} alt="" />
+        <img src={more_icon} alt="" />
+        <img src={notification_icon} alt="" />
+        <img src={profile_icon} className="user-icon" alt="" />
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;

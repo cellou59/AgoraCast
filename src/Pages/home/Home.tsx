184867cb7081e90cb3bf43ad780b4968a4c9a304
cellouRetailShake <cellu@retailshake.com>
2024-07-15 15:06:52 +0200

    feat: 🎸 Update Sidebar to handle category selection
    - Added category state and setCategory function to Sidebar component
    
    BREAKING CHANGE: 🧨 - Updated Home component to pass category state to Feed component

--- src/Pages/home/Home.tsx
+++ src/Pages/home/Home.tsx
@@ -1,14 +1,17 @@
-// import React from 'react'
+import { useState } from 'react'
+
 import './Home.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import Feed from '../../Components/Feed.tsx/Feed'
 
 function Home({sidebarOpen} : {sidebarOpen: boolean}) {
+
+  const [category, setCategory] =useState<number>(0)
   return (
     <>
-    <Sidebar sidebarOpen={sidebarOpen} />
+    <Sidebar sidebarOpen={sidebarOpen} category={category} setCategory={setCategory}/>
     <div className={`container ${sidebarOpen ? "" :"large-container"}`}>
-      <Feed />
+      <Feed category={category} />
     </div>
     </>
   )

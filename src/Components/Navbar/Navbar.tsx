e1f5aae897c26c6da0d47b3fad825a572ecdb2eb
cellouRetailShake <cellu@retailshake.com>
2024-07-17 17:49:46 +0200

    style: 💄 change some color

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "./Navbar.css";
 import menu_icon from "../../assets/menu.png";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo_sm.png";
+import mescena from "../../assets/mescena.png";
 import search_icon from "../../assets/search.png";
-import upload_icon from "../../assets/upload.png";
-import more_icon from "../../assets/more.png";
 import notification_icon from "../../assets/notification.png";
 import profile_icon from "../../assets/jack.png";
 import { Link } from "react-router-dom";
@@ -25,6 +24,7 @@ function Navbar({
         />
         <Link to="/">
           <img className="logo" src={logo} alt="" />
+          <img className="logo_name" src={mescena} alt="" />
         </Link>
       </div>
       <div className="nav-middle flex-div">
@@ -34,8 +34,6 @@ function Navbar({
         </div>
       </div>
       <div className="nav-right flex-div">
-        <img src={upload_icon} alt="" />
-        <img src={more_icon} alt="" />
         <img src={notification_icon} alt="" />
         <img src={profile_icon} className="user-icon" alt="" />
       </div>
